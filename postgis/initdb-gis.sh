8658f81bca3bdf7c8f392bc50e61c1b1a05c9149
luca-saggese <luca.saggese@gmail.com>
2017-03-02 17:22:46 +0100

    ff

--- postgis/initdb-gis.sh
+++ postgis/initdb-gis.sh
@@ -1,15 +1,5 @@
 #! /bin/sh
 
-echo "Initialization of postgis extensions and dropping default database \"gis\","
-
-dropdb -U postgres gis
-createuser tileserver -s --superuser # answer yes for superuser (although this isn't strictly necessary)
-createdb -E UTF8 -O tileserver gis
-
-
-sudo -u postgres psql
-\c gis
-CREATE EXTENSION postgis;
-ALTER TABLE geometry_columns OWNER TO tileserver;
-ALTER TABLE spatial_ref_sys OWNER TO tileserver;
-\q
+echo "Skipping initialization of postgis extensions and dropping default database \"nominatim\","
+echo "because nominatim setup script will take care of initialization later."
+dropdb -U tileserver gis
